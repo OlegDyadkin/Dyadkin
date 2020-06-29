39a3ac1c91fb1b737c63595f8c0e0d28831afee4
odyadk <oleg.dyadkin@mera.com>
2020-06-30 00:48:20 +0300

    Task#2 MagicNumbs and Opposite

---
+++ 2/Opposite.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class Opposite {
+    public static void main(String[] args) {
+        int[] numbersArray;
+        numbersArray = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < numbersArray.length; i++) {
+            numbersArray[i] = random.nextInt();
+            System.out.println(numbersArray[i]);
+        }
+        for (int i = 0; i < numbersArray.length; i++) {
+            for (i = 0; i < numbersArray.length - 1; i++) {
+                if (((numbersArray[i] < 0) && (numbersArray[i + 1] > 0)) || ((numbersArray[i] > 0) && (numbersArray[i + 1] < 0))) {
+                    numbersArray[i] += numbersArray[i + 1];
+                    numbersArray[i + 1] = numbersArray[i];
+                    break;
+                }
+            }
+
+        }
+        System.out.println("Преобразованный массив");
+        for (int i = 0; i < numbersArray.length; i++) {
+            System.out.println(numbersArray[i]);
+        }
+    }
+}
